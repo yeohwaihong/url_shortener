6fa04d7218d2de578892edd2263c8bcfd2c9832c
yeohwaihong <yeohwaihong@gmail.com>
2017-05-25 11:28:01 +0800

    able to shorten url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,8 +3,31 @@ class UrlsController < ApplicationController
     @urls = Url.all
   end
 
+
   def show
-    byebug
     @url = Url.find(params[:id])
   end
+
+
+  def new
+    @url = Url.new
+  end
+
+
+  def create
+    @url = Url.new(url_params)
+    @url.shorten
+    if @url.save
+        redirect_to @url
+    else
+        render 'new'
+    end
+  end
+
+
+  private
+  def url_params
+      params.require(:url).permit(:long_url)
+  end
+
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ApplicationRecord
+validates :long_url, uniqueness:true, presence:true, format: { with: /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/, message: "Not a valid URL"}
+
+  def shorten
+    self.short_url = SecureRandom.hex(3)
+  end
+
 end
